df6c9896b00ff87b8945228d8943fb35e90c264b
Farzan Khan <farzankhan009@gmail.com>
2021-12-20 19:52:52 +0500

    spaces rem

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const Product = require('./models/product-model.js');
-
-const {
-  body,
-  validationResult
-} = require('express-validator');
-
-// const Validator = require("fastest-validator")
-
+// const mongoose = require("mongoose")
 const app = express();
 
 app.use(bodyParser.urlencoded({
@@ -17,19 +8,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-
-
-
-
-
-
 require('./routes/routes.js')(app);
-
-
-
-
-
-
 app.listen(3000, function() {
   console.log("Server Started on Port 3000")
 });
